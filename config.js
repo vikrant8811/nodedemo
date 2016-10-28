0fba0485fb1c058f1ea0bb97c3f937121d52be15
Vikky Patel <vikrant8811@gmail.com>
2016-10-28 18:54:30 +0530

    us/ps

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ module.exports = {
 	is_hosted: false,
 	redis: {
 		host: 'redis-15288.c10.us-east-1-3.ec2.cloud.redislabs.com',
-		port: 15288
+		port: 15288,
+        password: 'ngtask',
+        db: 'ngtask'
 	}
 };
